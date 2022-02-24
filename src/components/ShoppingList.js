425d579d42fbb4f6f46f5838ca584a824a046d1f
princegobah <babynamek@gmail.com>
2022-02-24 16:07:21 -0500

    commit tree 0c45458756a62ee1049bbdded89f18c992b2e5bd

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,21 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+
+const [selectedCategory, setSelectedCategory]=useState("All")
+
+function handleChange(event){
+  setSelectedCategory(event.target.value)
+
+}
+
+  const filterIt=items.filter(item=>item.category===selectedCategory)
+
+const allItems=items.map(
+  (item) => (
+   
+    <Item key={item.id} name={item.name} category={item.category} />
+  ))
+
+ const filterMap= filterIt.map(
+  (item) => (
+   
+    <Item key={item.id} name={item.name} category={item.category} />
+  ))
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
+        <select name="filter"
+        onChange={handleChange}
+        >
+          <option value="All"
+     
+          >Filter by category</option>
+          <option 
+          value="Produce"
+          >Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+      {selectedCategory==="All"?allItems:filterMap}
+        
       </ul>
     </div>
   );
